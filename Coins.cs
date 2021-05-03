ec5e0e910e1b224f56ca836269080a081e1871dc
jennyraj <jennyraj@users.noreply.github.com>
2021-05-03 09:10:47 -0400

    Add files via upload
    CS files added

---
+++ Coins.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Windows.Markup;
+
+namespace ConsoleApp2
+{
+       public class Coins
+    {
+        public int minReplacement(int[] A)
+        {
+            //Two possible choices
+            //A first element is 1 or first element is 0
+
+            var x1 = CountFlips(A, 1);
+            var x2 = CountFlips(A, 0);
+            return Math.Min(x1, x2);
+        }
+
+        private static int CountFlips(int[] A, int expected)
+        {
+            var flips = 0;
+            int notExpected = expected == 1 ? 0 : 1;
+            for (int i = 0; i < A.Length; i++)
+            {
+                // If there is expected at even index positions
+                // OR If there is notExpected at odd index positions
+                if ((i % 2 == 0 && A[i] == expected) ||
+                    (i % 2 == 1 && A[i] == notExpected)
+                )
+                {
+                    flips++;
+                }
+            }
+
+            return flips;
+        }
+    }
+}
---
+++ SplitString.cs
@@ -0,0 +1,45 @@
+﻿using System.Linq;
+
+namespace ConsoleApp2
+{
+    public class StringTest
+    {
+        // Function to calculate // the number of ways to split
+        public int SplitString(string s)
+        {
+            var n = s.Length;
+
+            //total number of a's
+            var count = s.Count(x => x == 'a');
+
+            //trivial cases
+            if (count % 3 != 0) //not a multiple of 3
+                return 0;
+
+            if (count == 0) //a not found, can be split anyway we want
+                return (n - 1) * (n - 2) / 2;
+
+            //General case-------------
+            //string is cut in two places to make 3 parts.
+
+            var charCount = count / 3;
+            var cut1 = 0;
+            var cut2 = 0;
+
+            var numA = 0; //number of A's
+            for (var i = 0; i < n; i++)
+            {
+                if (s[i] == 'a')
+                    numA++;
+
+                if (numA == charCount)
+                    cut1++;
+
+                if (numA == 2 * charCount)
+                    cut2++;
+            }
+
+            return cut1 * cut2;
+        }
+    }
+}
